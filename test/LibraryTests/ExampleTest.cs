27fd59cffeb1ea50ce9ab62c845642825a572f7e
Macarenapineiro1 <macarena.pineiro@correo.ucu.edu.uy>
2024-09-18 16:46:30 -0300

    Correcion

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -7,7 +7,7 @@ public class Tests
     [Test]
     public void TestAttackValue()
     {
-        const string name1 = "Elgifo 1";
+        const string name1 = "Elfo 1";
         const string name2 = "Elfo 2";
         Elf elf1 = new Elf(name1);
         Elf elf2 = new Elf(name2);
