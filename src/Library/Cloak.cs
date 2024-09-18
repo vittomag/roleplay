eb74c95c1a269ad2244f3a9834a3b8d0afbf2a65
TiagoAris <aristimuniotiago@gmail.com>
2024-09-18 19:46:41 -0300

    agregue clase wizard, spellbook y cloak

---
+++ src/Library/Cloak.cs
@@ -0,0 +1,13 @@
+﻿namespace Library;
+
+public class Cloak
+{
+    public string Name {get; set;}
+    public int DefenseValue{get; set;}
+    public Cloak(string name)
+    {
+        this.Name = name;
+        this.DefenseValue = 10;
+    }
+
+}
---
+++ src/Library/Wizard.cs
@@ -0,0 +1,70 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Library;
+
+public class Wizard
+{ 
+    public string Name{get; set;} 
+    
+    public int Health{get; set;}
+    public int Mana{get; set;}
+    
+    public Wizard(string name) 
+    { 
+        this.Name = name;
+        this.Health = 100;
+    }
+
+    public SpellBook SpellBook { get; set; }
+    public Cloak Cloak { get; set; }
+    
+    public int GetAttackValue()
+    {
+        if (this.SpellBook != null)
+        {
+            int attack = SpellBook.AttackValue;
+            return attack;
+        }
+        else
+        {
+            return 10;
+        }
+    }
+    
+    public int GetDefenseValue()
+    {
+        if (this.Cloak != null)
+        {
+            int defense = Cloak.DefenseValue;
+            return defense;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+    public int AttackOther(Wizard wizard) 
+    {
+        int attackValue = this.GetAttackValue();
+        int defenseValue = wizard.GetDefenseValue();
+        wizard.Health -= attackValue - defenseValue;
+        return wizard.Health;
+    }
+    
+    public int Heal(Wizard wizard)
+    {
+        if (this.Health < 100)
+        {
+            this.Health += 50; 
+            if (this.Health > 100)
+            {
+                int resta = this.Health - 100;
+                this.Health -= resta;
+            }
+        }
+        
+        return this.Health;
+    }
+    
+}
