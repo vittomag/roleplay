c7c0a100ffb35811edab0eaddcde9eb1415b1fe7
Macarenapineiro1 <macarena.pineiro@correo.ucu.edu.uy>
2024-09-23 12:53:16 -0300

    Test

--- test/LibraryTests/TestElf.cs
+++ test/LibraryTests/TestElf.cs
@@ -35,7 +35,25 @@ public class TestElf
         int defense = bow.DefenseValue + ring.DefenseValue;
         Assert.AreEqual(defense, elfo.DefenseValue);  
     }
+    [Test]
+    public void TestAttackDwarf()
+    {
+        const string name = "Elfo";
+        Elf elfo = new Elf(name);
+        Bow bow = new Bow("Bow");
+        Ring ring = new Ring("Ring");
+        elfo.Bow = bow;
+        elfo.Ring = ring;
+        const string name1 = "Enano";
+        Dwarf dwarf = new Dwarf(name1);
+        Shield shield = new Shield("Shield");
+        Hammer hammer = new Hammer("Hammer");
+        dwarf.Hammer = hammer;
+        dwarf.Shield = shield;
+        int expectedhealth = elfo.Health - (hammer.AttackValue - ring.DefenseValue);
+        elfo.AttackOther(dwarf);
+        Assert.AreEqual(elfo.Health, expectedhealth);
+    }
 
 
-    
 }
