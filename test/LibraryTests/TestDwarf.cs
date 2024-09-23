c77d2bb6cc9d061b099e46c07be5129fcb3c2f63
Vittorio M <brunomag222@gmail.com>
2024-09-23 11:52:09 -0300

    test

--- test/LibraryTests/TestDwarf.cs
+++ test/LibraryTests/TestDwarf.cs
@@ -8,17 +8,32 @@ public class TestDwarf
     [Test]
     public void TestAttackValue()
     {
-        const string name1 = "Enano 1";
-        const string name2 = "Enano 2";
-        Dwarf dwarf1 = new Dwarf(name1);
-        Dwarf dwarf2 = new Dwarf(name2);
-        dwarf1.Hammer = new Hammer("Hammer");
-        dwarf2.Shield = new Shield("Shield");
-        int initialhealth = dwarf2.Health;
-        dwarf1.AttackOther(dwarf2);
-        int expecteddamage = dwarf1.GetAttackvalue() - dwarf2.GetDefensevalue();
-        int expectedhealth = initialhealth - expecteddamage;
-        Assert.AreEqual(expectedhealth, dwarf2.Health);   
-
+        const string name = "Enano";
+        Dwarf enano = new Dwarf(name);
+        Hammer hammer = new Hammer("Hammer");
+        Shield shield = new Shield("Shield");
+        enano.Hammer = hammer;
+        enano.Shield = shield;
+      
+        int attack = hammer.AttackValue + shield.AttackValue;
+        Assert.AreEqual(attack, enano.AttackValue);
+      
     }
+
+
+    [Test]
+    public void TestDeffenseValue()
+    {
+        const string name = "Enano";
+        Dwarf enano = new Dwarf(name);
+        Hammer hammer = new Hammer("Hammer");
+        Shield shield = new Shield("Shield");
+        enano.Hammer = hammer;
+        enano.Shield = shield;
+      
+        int defense = hammer.DefenseValue + shield.DefenseValue;
+        Assert.AreEqual(defense, enano.DefenseValue);  
+    }
+
+
 }
