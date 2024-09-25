0dd81694f0dbdaa5cca01eb4cd333e377c765157
Macarenapineiro1 <macarena.pineiro@correo.ucu.edu.uy>
2024-09-25 12:57:19 -0300

    Program

--- src/Library/Spell.cs
+++ src/Library/Spell.cs
@@ -3,4 +3,20 @@
 public class Spell
 {
     public string Name{get; set;}
+
+    public int AttackValue
+    {
+        get
+        {
+            return 70;
+        }
+    }
+
+    public int DefenseValue
+    {
+        get
+        {
+            return 0;
+        }
+    }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Library;
+
+Elf elf = new Elf("Elfo");
+elf.Bow = new Bow("Bow");
+elf.Ring = new Ring("Ring");
+Dwarf dwarf = new Dwarf("Enano");
+dwarf.Shield = new Shield("Shield");
+dwarf.Hammer = new Hammer("Hammer");
+
+Console.WriteLine($"Elfo tiene {elf.Health} de vida");
+Console.WriteLine($"Enano ataca a Elfo con {dwarf.AttackValue} de ataque");
+elf.AttackOther(dwarf);
+Console.WriteLine($"Elfo tiene {elf.Health} de vida");
+elf.Heal();
+Console.WriteLine($"Elfo tiene {elf.Health} de vida");
